d58f8975613071b8fff9d144c300a23539ac792e
Ossi H <50570717+ossi1801@users.noreply.github.com>
2021-01-20 22:25:38 +0200

    reveal animation snippet

---
+++ React.js tests/reveal.js
@@ -0,0 +1,19 @@
+// You can live edit this code below the import statements
+//https://www.react-reveal.com/
+//
+import React from 'react';
+import LightSpeed from 'react-reveal/LightSpeed';
+
+class LightSpeedExample extends React.Component {
+  render() {
+    return (
+      <div>
+        <LightSpeed left>
+          <h1>React Reveal</h1>
+        </LightSpeed>
+      </div>
+    );
+  }
+}
+
+export default LightSpeedExample;
